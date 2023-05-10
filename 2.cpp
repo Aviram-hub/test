2c99d25a838243fa3e0fcb3dde6ba9f266832f5e
Kunqing Deng <dengkunqing@scutech.com>
2023-05-10 13:57:22 +0800

    second

---
+++ 2.cpp
@@ -0,0 +1,6 @@
+
+
+// vim: set ts=4 sw=4 sts=4 et:
+右边窗口修改
+fsdfdsg
+df
--- aa.cpp
+++ aa.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 using namespace std;
 int main() {
-
+    cout << "hello world" << endl;
+    return 0;
 }
 
 // vim: set ts=4 sw=4 sts=4 et:
+
+// ffdds
+
+// haha hello
+
+左边窗口修改
+dsad
+fsdf
